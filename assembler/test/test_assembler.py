8a3f8050568d44ca67e0f4d42374cecb15d48862
Ammar Askar <ammar@ammaraskar.com>
2021-11-12 16:28:39 -0500

    Test each single register type instruction

--- assembler/test/test_assembler.py
+++ assembler/test/test_assembler.py
@@ -8,8 +8,14 @@ def test_load_a_instruction():
     assert assembly == ['0000000000000011']
 
 
-def test_c_instruction_with_only_alu_output():
+def test_c_instruction_with_single_register_a():
     ast = assembler.parse_and_validate_ast('D = A')
-    assembly = assembler.assemble_ast(ast)
+    assert assembler.assemble_ast(ast) == ['1110110000010000']
 
-    assert assembly == ['1110110000010000']    
+def test_c_instruction_with_memory():
+    ast = assembler.parse_and_validate_ast('A = *A')
+    assert assembler.assemble_ast(ast) == ['1111110000100000']    
+
+def test_c_instruction_with_single_register_d():
+    ast = assembler.parse_and_validate_ast('D, *A = D')
+    assert assembler.assemble_ast(ast) == ['1110001100011000']
